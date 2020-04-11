5cf0aa698709ec466f8a582d1b92415685796e06
LeXuanThang-UTC <61774990+LeXuanThang-UTC@users.noreply.github.com>
2020-04-11 21:40:52 +0700

    update version 0.2.0
    expand your range in guessing this game

--- randomguessgame.py
+++ randomguessgame.py
@@ -1,28 +1,34 @@
 from random import randint
 import sys
+
 ## HI EVERYONE IS THE COMPLETE RANDOM GUESSING GAME I MADE
 ## BASED ON ANDERI NEAGOIE
-# I MADE EXIT CHOICE TO EXIT THIT GAME FROM WHILE LOOP.
+# I MADE EXIT CHOICE TO EXIT THIS GAME FROM WHILE LOOP.
 # play it with terminal window
 # generate a number 1 ~ 10
-answer = randint(int(sys.argv[1]), int(sys.argv[2]))
+lower = int(sys.argv[1])
+upper = int(sys.argv[2])
 
 while True:
-# input guess
-    guess_answer = input('Lets play. Enter your number 1 ~ 10: ')
+    # input guess
+    answer = randint(lower, upper)
+    guess_answer = input(f'Lets play. Enter your number {lower} ~ {upper}: ')
     if guess_answer == 'exit':
         print('Bye bye')
         break
     guess_answer = int(guess_answer)
     try:
         # check that input is a number 1 ~ 10
-        if 0 < guess_answer < 11:
+        if lower < guess_answer < upper:
             if guess_answer == answer:
                 print('omg. why are you so smart!!! :D')
                 continue
             else:
                 print('uhm. It is so close. do you want to try again')
                 continue
-    except ValueError:
-            print('opps. try enter right number in this game :')
+        else:
+            print(f'omg. Enter your number {lower}~ {upper}:')
             continue
+    except ValueError:
+        print('oops. try enter right number in this game :')
+        continue
